5bb33c0254c8bd3c3e3b31ada4e44bbbcd7e37c8
francelmo <francelmo87@hotmail.com>
2025-11-20 10:00:17 -0300

    created the DB:Save the datas

--- app/tanks/models.py
+++ app/tanks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class WaterLevel(models.Model):
+    value = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.value} - {self.created_at}"
+
